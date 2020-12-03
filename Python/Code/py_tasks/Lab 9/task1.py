0df85ce4adcb7958d9e5994fcba25ddd7a120086
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-12-03 14:48:33 +0000

    lab 9 exception handling

--- Python/Code/py_tasks/Lab 9/task1.py
+++ Python/Code/py_tasks/Lab 9/task1.py
@@ -1,12 +1,24 @@
-from math import gcd
+from math import gcd, nan
 
 class Fraction(object):
-    def __init__(self, num, denom):
-        g = gcd(num, denom)
-        num = int(num/g)
-        denom = int(denom/g)
-        self.num = num
-        self.denom = denom
+    def __init__(self, num, denom):    
+        try:
+            assert denom != 0
+            g = gcd(num, denom)
+            num = int(num/g)
+            denom = int(denom/g)
+           
+            self.num = num
+            self.denom = denom
+            
+        except AssertionError:
+            print("Fraction denominator cannot be zero! (Division by 0 is impossible)")
+            return None
+       
+        except TypeError:
+            print("Inputs are not integer values! (Must be integers)")
+            return None
+        
 
     def __str__(self):
         return str(self.num) + "/" + str(self.denom)
@@ -40,7 +52,10 @@ class Fraction(object):
     def scale(fraction_list, factor):
         scaled = []
         for i in range(len(fraction_list)):
-            scaled.append(str(fraction_list[i] / Fraction(int(factor), 1)))
+            try:
+                scaled.append(str(fraction_list[i] / Fraction(int(factor), 1)))
+            except ZeroDivisionError:
+                scaled.append(nan)
         return scaled
 
     def printListOfFractions(fraction_list):
@@ -57,6 +72,8 @@ f2 = Fraction(1, 3)
 f3 = Fraction(2, 4)
 f4 = Fraction(1, 4)
 f = f1+f2
+f6 = Fraction(1, 0)
+f5 = Fraction("string", "hello")
 
 f_list = [f1, f2, f3, f4]
 
